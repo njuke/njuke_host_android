c6e29394ecdfb49fbddd24847775cc60013c6709
Erik Jansson <erikjansson90@gmail.com>
2014-08-27 15:18:53 +0200

    Replace manual swipe with standard ViewPager.

--- app/src/main/java/njuke/njuke_host/OverviewTabFragment.java
+++ app/src/main/java/njuke/njuke_host/OverviewTabFragment.java
@@ -3,7 +3,7 @@ package njuke.njuke_host;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +30,5 @@ public class OverviewTabFragment extends Fragment {
         spinner.setAdapter(adapter);
         return view;
     }
-
 
 }
--- app/src/main/java/njuke/njuke_host/main.java
+++ app/src/main/java/njuke/njuke_host/main.java
@@ -1,23 +1,70 @@
 package njuke.njuke_host;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.widget.TabHost;
+
+import java.util.Locale;
 
 
-public class main extends Activity {
+public class main extends FragmentActivity implements ActionBar.TabListener {
+    /* Debug tag. */
+    public static final String TAG = main.class.getSimpleName();
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        if (actionBar == null) {
+            Log.e(TAG, "Failed to get ActionBar. Bailing!");
+            return;
+        }
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            actionBar.addTab(actionBar.newTab()
+                    .setText(mSectionsPagerAdapter.getPageTitle(i))
+                    .setTabListener(this));
+        }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,4 +84,66 @@ public class main extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private static final int NUM_PAGES = 3;
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // FIXME: This could probably be done cleaner.
+            switch (position) {
+                case 0:
+                    return new OverviewTabFragment();
+                case 1:
+                    return new PlaylistTabFragment();
+                default:
+                    // TODO: Add last fragment.
+                    return new PlaylistTabFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public CharSequence getPageTitle(final int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.overviewTabName).toUpperCase(l);
+                case 1:
+                    return getString(R.string.playlistTabName).toUpperCase(l);
+                case 2:
+                    return getString(R.string.votersTabName).toUpperCase(l);
+                default:
+                    return "";
+            }
+        }
+    }
+
 }
